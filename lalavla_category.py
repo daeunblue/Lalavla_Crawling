740b837584953bd912c666005fa8d0c900bc8dd5
daeunblue <57705970+daeunblue@users.noreply.github.com>
2021-03-10 15:30:15 +0900

    Add files via upload

---
+++ lalavla_category.py
@@ -0,0 +1,237 @@
+category_list = {
+  'skincare': {
+    'basic': {
+      'all': 'C090300',
+      'set': 'C090300',
+      'skin_toner': 'C090300',
+      'lotion': 'C090300',
+      'essence_serum_ample': 'C090300',
+      'cream': 'C090300',
+      'eyecream': 'C090300',
+      'mist': 'C090300',
+      'faceoil': 'C090300'
+    },
+    'mask': {
+      'all': 'C090100',
+      'sheet': 'C090100',
+      'washoff_sleeping': 'C090100',
+      'patch': 'C090100',
+      'partial': 'C090100',
+      'nose': 'C090100'
+    },
+    'cleanser': {
+      'all': 'C090400',      
+      'foam_gel': 'C090400',
+      'oil': 'C090400',
+      'water': 'C090400',
+      'remover': 'C090400',
+      'pad_tissue': 'C090400',
+      'peeling_scrub': 'C090400',
+      'cream': 'C090400'
+    },
+    'suncare': {
+      'all': 'C10000000087',
+      'cream_lotion': 'C10000000087',
+      'stick': 'C10000000087',
+      'cushion_spray': 'C10000000087',
+      'tanning_aftersun': 'C10000000087'
+    }
+  },
+  'makeup': {
+    'lip': {
+      'all': 'C10000000080',
+      'tint_gloss': 'C10000000080',
+      'stick': 'C10000000080',
+      'care': 'C10000000080'
+    },
+    'base': {
+      'all': 'C040300',
+      'base_primer': 'C040300',
+      'bb_cc': 'C040300',
+      'foundation': 'C040300',
+      'cushion': 'C040300',
+      'powder_pact': 'C040300',
+      'concealer': 'C040300'
+    },
+    'eye': {
+      'all': 'C040400',
+      'mascara': 'C040400',
+      'liner': 'C040400',
+      'brow': 'C040400',
+      'shadow': 'C040400'
+    },
+    'contouring': {
+      'all': 'C10000000097',
+      'blusher': 'C10000000097',
+      'shading_highlighter': 'C10000000097'
+    },
+    'nail': {
+      'all': 'C040100',
+      'tip_sticker': 'C040100',
+      'care_remover': 'C040100',
+      'normal': 'C040100',
+    }
+  },
+  'bodycare': {
+    'lotion': {
+      'all': 'C060100',
+      'lotion': 'C060100',
+      'cream_oil': 'C060100',
+      'mist': 'C060100',
+    },
+    'cleanser': {
+      'all': 'C060200',
+      'wash': 'C060200',
+      'scrub': 'C060200',
+      'soap_bathbomb': 'C060200'
+    },
+    'wax_deo': {
+      'all': 'C060300',
+      'women': 'C060300',
+      'cream_strip': 'C060300',
+      'deodorant': 'C060300'
+    },
+    'hand_foot': {
+      'all': 'C060400',
+      'footcare_cream': 'C060400',
+      'handcare_cream': 'C060400'
+    }
+  },
+  'haircare': {
+    'shampoo': {
+      'all': 'C110100'
+    },
+    'condi_treatment': {
+      'all': 'C10000000077'
+    },
+    'color': {
+      'all': 'C10000000078'
+    },
+    'essence_oil': {
+      'all': 'C10000000102'
+    },
+    'styling': {
+      'all': 'C10000000102',
+      'wax': 'C10000000079',
+      'gel_spray': 'C10000000079'
+    }
+  },
+  'beauty_health_tool': {
+    'beauty_tool': {
+      'all': 'C080100',
+      'oilpaper': 'C080100',
+      'curler_eyelash': 'C080100',
+      'puff_sponge': 'C080100',
+      'cottonpad_swab': 'C080100',
+      'bottle': 'C080100',
+      'brush': 'C080100',
+      'etc': 'C080100',
+      'tissue_water': 'C080100'
+    },
+    'device': {
+      'all': 'C080200',
+      'skincare': 'C080200',
+      'hair': 'C080200'
+    },
+    'hair_tool': {
+      'all': 'C080400',
+      'brush': 'C080400',
+      'acc': 'C080400'
+    },
+    'health_tool': {
+      'all': 'C10000000111'
+    },
+  },
+  'health_hygiene': {
+    'oral': {
+      'all': 'C010100',
+      'cleaner': 'C010100',
+      'etc': 'C010100',
+      'floss_spray': 'C010100',
+      'toothpaste': 'C010100',
+      'toothbrush': 'C010100'
+    },
+    'eyecare': {
+      'all': 'C010200',
+      'lens': 'C010200',
+      'eyemask': 'C010200'
+    },
+    'feminine': {
+      'all': 'C010300',
+      'pad': 'C010300',
+      'tampon_cup': 'C010300',
+      'cleaner': 'C010300'
+    },
+    'medical': {
+      'all': 'C010400',
+      'mask_dust': 'C010400',
+      'band_emergency': 'C010400',
+      'insect': 'C010400',
+      'adult': 'C010400'
+    }
+  },
+  'fragrance': {
+    'perfume': {
+      'all': 'C100200',
+      'women': 'C100200',
+      'men': 'C100200'
+    },
+    'fabric_room': {
+      'all': 'C100100',
+      'diffuser_candle': 'C100100',
+      'fabric_perfume': 'C100100'
+    }
+  },
+  'men': {
+    'skin_body': {
+      'all': 'C020300'
+    },
+    'shaving': {
+      'all': 'C020200'
+    },
+    'makeup': {
+      'all': 'C10000000085'
+    }
+  },
+  'health_food': {
+    'healthfood': {
+      'all': 'C120100',      
+      'diet': 'C120100'
+    },
+    'vitamin': {
+      'all': 'C10000000113'
+    }
+  },
+  'normal_food': {
+    'snack_cookie': {
+      'all': 'C10000000115'
+    },
+    'choco_candy_jelly': {
+      'all': 'C10000000116'
+    },
+    'nuts': {
+      'all': 'C10000000117'
+    },
+    'health': {
+      'all': 'C10000000118'
+    },
+    'coffee_tea': {
+      'all': 'C10000000119'
+    },
+    'water_drink': {
+      'all': 'C10000000120'
+    },
+    'convenience': {
+      'all': 'C10000000123'
+    }
+  },
+  'etc': {
+    'design_app': {
+      'all': 'C030300'
+    },
+    'living': {
+      'all': 'C030400',
+      'airfreshener': 'C030400'
+    }
+  }
+}
